cce5c299fd9d504e0db51a93b59a54d59ace156b
fpgutierrez <federingutierrez@gmail.com>
2026-07-24 00:46:55 -0500

    Avances

--- src/Core.Idempotency/Constants/HeaderNames.cs
+++ src/Core.Idempotency/Constants/HeaderNames.cs
@@ -2,5 +2,7 @@
 
 internal static class HeaderNames
 {
+    public const string IdempotencyKey = "Idempotency-Key";
+
     public const string IdempotencyCache = "X-Idempotency-Cache";
 }
---
+++ src/Core.Idempotency/Models/IdempotencyEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Idempotency.Models;
+
+public sealed class IdempotencyEntry
+{
+    public string? Fingerprint { get; init; }
+
+    public required IdempotencyResponse Response { get; init; }
+}
